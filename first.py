5d11b59a19ab4b85fd4ac8ce06fbe33a0c8bd63b
finalSay9 <chimwazaevan1@gmail.com>
2026-07-20 08:23:16 +0200

    initializing the deep learning project

--- first.py
+++ first.py
@@ -1,3 +1,50 @@
-import tensorflow as tf
+import keras
 
-print(tf.__version__)
+
+class MyDense(keras.layers.Layer):
+    def __init__(self, units, activation=None, name=None):
+        super().__init__(name=name)
+        self.units = units
+        self.activation = keras.activations.get(activation)
+
+    def build(self, input_shape):
+        input_dim = input_shape[-1]
+        self.w = self.add_weight(
+            shape=(input_dim, self.units),
+            initializer=keras.initializers.GlorotNormal(),
+            name="kernel",
+            trainable=True,
+        )
+
+        self.b = self.add_weight(
+            shape=(self.units,),
+            initializer=keras.initializers.Zeros(),
+            name="bias",
+            trainable=True,
+        )
+
+    def call(self, inputs):
+        # Use Keras ops to create backend-agnostic layers/metrics/etc.
+        x = keras.ops.matmul(inputs, self.w) + self.b
+        return self.activation(x)
+
+class MyModel(keras.Model):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv_base = keras.Sequential(
+            [
+                keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+                keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+                keras.layers.MaxPooling2D(pool_size=(2, 2)),
+                keras.layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+                keras.layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+                keras.layers.GlobalAveragePooling2D(),
+            ]
+        )
+        self.dp = MyDropout(0.5)
+        self.dense = MyDense(num_classes, activation="softmax")
+
+    def call(self, x):
+        x = self.conv_base(x)
+        x = self.dp(x)
+        return self.dense(x)
