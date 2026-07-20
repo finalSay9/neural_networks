1222f385eb44203d90229a1d2110f8af435f181f
finalSay9 <chimwazaevan1@gmail.com>
2026-07-20 09:34:01 +0200

    layers been created

---
+++ layers.py
@@ -0,0 +1,32 @@
+
+#lets create a layer this time
+
+inputs = [2, 1.2, 3, 4.0]
+
+weights1 = [0.1, 0.3, -0.4, 0.4]
+weights2 = [0.2, 0.1, 1, 2],
+weights3 = [1, 2, -0.5, -0.1]
+bias1 = 2
+bias2 = 1
+bias3 = 3
+
+outputs = ((
+    inputs[0]*weights1[0] +
+    inputs[1]*weights1[1] +
+    inputs[2]*weights1[2] +
+    inputs[3]*weights1[3] + bias1
+) +
+  (
+   inputs[0]*weights2[0] +
+   inputs[1]*weights2[1] +
+   inputs[2]*weights2[2] +
+   inputs[3]*weights2[3] + bias2
+  )
+  +
+  (
+   inputs[0]*weights3[0] +
+   inputs[1]*weights3[1] +
+   inputs[2]*weights3[2] +
+   inputs[3]*weights3[3] + bias3
+  )
+)
