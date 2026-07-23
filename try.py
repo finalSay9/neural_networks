e6068daca2f749a38cc57b881d2c611e26d106d6
finalSay9 <chimwazaevan1@gmail.com>
2026-07-23 11:00:22 +0200

    creating and initialzing the optical character recognition

--- try.py
+++ try.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 inputs = [1.2, 2, 1.0, 3]
-weights = [0.1, 1.1, 0.2, 0.3]
-bias = 2
+weights = [
+    [0.1, 1.1, 0.2, 0.3],
+    [1.1, 0.4, 0.11, 0.2],
+    [0.1, 0.3, 0.2, 0.7]
+    ]
+bias = [0.2, 2.1, 3]
 
 output = np.dot(inputs, weights) + bias
 
