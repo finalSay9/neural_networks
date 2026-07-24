0a5ddff39479e71f229fbeb8a6dabb11a8d682a2
finalSay9 <chimwazaevan1@gmail.com>
2026-07-24 08:29:30 +0200

    raising the value error

--- class.py
+++ class.py
@@ -1,5 +1,4 @@
 import math
-
 class Circle:
     
     def __init__(self, radius):
@@ -16,3 +15,6 @@ class Circle:
         if not isinstance(value, int | float) or value <= 0:
             raise ValueError("Positive numbers expected")
         self._radius = value
+
+    def calculate_area(self):
+        return math.pi * self.radius ** 2
