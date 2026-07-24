cc769296f838b52f0b406f133b9de40ec5f267c6
finalSay9 <chimwazaevan1@gmail.com>
2026-07-24 08:27:39 +0200

    raising the value error

--- class.py
+++ class.py
@@ -5,7 +5,13 @@ class Circle:
     def __init__(self, radius):
         self.radius = radius
 
+
+     #turning radius into a property
     @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
     def radius(self, value):
         if not isinstance(value, int | float) or value <= 0:
             raise ValueError("Positive numbers expected")
